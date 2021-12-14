9d348123121981390fcda1a0833cdb4e88fa17b8
GUNDUZ_Maxime_Ziya <75846767+MaximeZiyaGunduz@users.noreply.github.com>
2021-12-14 12:24:30 +0100

    Delete Jeu d'echec en Java directory

--- Jeu d'echec en Java/Echec projet/src/Roi.java
+++
@@ -1,9 +0,0 @@
-public class Roi extends Piece {
-    public Roi(String couleur) {
-        super(couleur);
-        if(couleur == BLANC)
-            super.setSymbole(Roi_Blanc);
-        else
-            super.setSymbole(Roi_Noir);
-    }
-}
--- Jeu d'echec en Java/README.md
+++
@@ -1,2 +0,0 @@
-# Projet
-Voici notre projet Java
